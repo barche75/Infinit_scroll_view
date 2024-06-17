7c2b1468df42d2265b1cfcd2ef84395479ebcffa
Evgenii Kouzov <mouzov.ev@gmail.com>
2024-06-17 18:41:37 +0300

    Initial Commit

---
+++ InfiniteScrollView/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  InfiniteScrollView
+//
+//  Created by Евгений Коузов on 17.06.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ InfiniteScrollView/InfiniteScrollViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  InfiniteScrollViewApp.swift
+//  InfiniteScrollView
+//
+//  Created by Евгений Коузов on 17.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct InfiniteScrollViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
